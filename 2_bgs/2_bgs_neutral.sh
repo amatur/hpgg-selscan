302964fb60a30727eec09abf81a8e6110f6cd505
amatur <amatur003@gmail.com>
2025-08-05 20:26:18 -0400

    bgs

--- 2_bgs/2_bgs_neutral.sh
+++ 2_bgs/2_bgs_neutral.sh
@@ -1,8 +1,7 @@
-
 #!/bin/bash
-#SBATCH --job-name=1_1_constant_neutral       # Job name
-#SBATCH --output=../1_constant/%x.out        # Output file (%j will be replaced with the job ID)
-#SBATCH --error=../1_constant/%x.err          # Error file (%j will be replaced with the job ID)
+#SBATCH --job-name=2_bgs_neutral        # Job name
+#SBATCH --output=%x.out        # Output file (%j will be replaced with the job ID)
+#SBATCH --error=%x.err          # Error file (%j will be replaced with the job ID)
 #SBATCH --ntasks=1                    # Number of tasks (processes)
 #SBATCH --cpus-per-task=1            # Number of CPU cores per task
 #SBATCH --mem=8G                      # Memory per node (10GB in this case)
@@ -19,7 +18,6 @@ source $(conda info --base)/etc/profile.d/conda.sh
 conda activate test-slim
 
 echo "Starting job at $(date)"  # Print the current date
-cd ../1_constant
-mkdir -p ../output/1_1_constant_neutral
-slim 1_constant_neutral.slim
+mkdir -p ../output/2_2_bgs_neutral
+slim 2_bgs_neutral.slim
 echo "Ending job at $(date)" 
